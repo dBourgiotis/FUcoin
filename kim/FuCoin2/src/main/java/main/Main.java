a6715afbbe64df91ad7efc83a17734ec38a5317a
Kim Kern <kim.kern@fu-berlin.de>
2016-02-11 15:51:25 +0100

    main: final demo settings

--- kim/FuCoin2/src/main/java/main/Main.java
+++ kim/FuCoin2/src/main/java/main/Main.java
@@ -20,22 +20,24 @@ public class Main {
 		MainFrame main = new MainFrame(system);
 		main.setVisible(true);
 
-		UserControl kim = new UserControl(system, "Kim", 100);
+		// UserControl kim = new UserControl(system, "Kim", 100);
 		UserControl alex = new UserControl(system, "Alex", 1000);
-		UserControl dimi = new UserControl(system, "Dimi", 400);
-		UserControl berkay = new UserControl(system, "Berkay", 299);
+		UserControl dimi = new UserControl(system, "Dimi", 5);
+		UserControl berkay = new UserControl(system, "Berkay", 10);
 
 		Thread.sleep(1000L);
-		kim.addNewNeighbour("Alex", "akka://TestSystem/user/$b");
+		// kim.addNewNeighbour("Alex", "akka://TestSystem/user/$b");
 		// kim.addNewNeighbour("Alex", "akka.tcp://TestSystem@127.0.0.1:2552/user/$b");
-		dimi.addNewNeighbour("Alex", "akka://TestSystem/user/$b");
+		dimi.addNewNeighbour("Alex", "akka://TestSystem/user/$a");
+		berkay.addNewNeighbour("Alex", "akka://TestSystem/user/$a");
 		// dimi.addNewNeighbour("Alex", "akka.tcp://TestSystem@192.168.43.153:2552/user/$b");
-		kim.addNewNeighbour("Berkay", "akka://TestSystem/user/$d");
-		dimi.addNewNeighbour("Berkay", "akka://TestSystem/user/$d");
-		alex.addNewNeighbour("Dimi", "akka://TestSystem/user/$c");
-		berkay.addNewNeighbour("Dimi", "akka://TestSystem/user/$c");
-		alex.addNewNeighbour("Kim", "akka://TestSystem/user/$a");
-		berkay.addNewNeighbour("Kim", "akka://TestSystem/user/$a");
+		// kim.addNewNeighbour("Berkay", "akka://TestSystem/user/$d");
+		// dimi.addNewNeighbour("Berkay", "akka://TestSystem/user/$d");
+		alex.addNewNeighbour("Berkay", "akka://TestSystem/user/$c");
+		alex.addNewNeighbour("Dimi", "akka://TestSystem/user/$b");
+		// berkay.addNewNeighbour("Dimi", "akka://TestSystem/user/$c");
+		// alex.addNewNeighbour("Kim", "akka://TestSystem/user/$a");
+		// berkay.addNewNeighbour("Kim", "akka://TestSystem/user/$a");
 	}
 
 }
